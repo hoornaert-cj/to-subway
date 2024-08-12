752b296bb01d947431d055950a89f26b15ed3dec
hoornaert-cj <97850176+hoornaert-cj@users.noreply.github.com>
2024-08-12 13:41:35 -0700

    Added background map to the application

--- map.js
+++ map.js
@@ -1,2 +1,6 @@
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+var map = L.map('map').setView([43.66572016527307, -79.41484428761677], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 16,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
