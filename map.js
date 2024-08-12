28e0c10a52c30f060f6e3e0351b9fc0112331a41
hoornaert-cj <97850176+hoornaert-cj@users.noreply.github.com>
2024-08-12 13:52:05 -0700

    Added data\Green Spaces.geojson to the map display

--- map.js
+++ map.js
@@ -1,6 +1,15 @@
 var map = L.map('map').setView([43.66572016527307, -79.41484428761677], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 16,
+    maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+// Load GeoJSON from an external file
+fetch('data/Green Spaces.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data).addTo(map);
+    })
+    .catch(error => console.log('Error loading GeoJSON:', error));
