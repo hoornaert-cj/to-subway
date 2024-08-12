c1d0eb4ae2a262afff69b6d8f601f3bea147c931
hoornaert-cj <97850176+hoornaert-cj@users.noreply.github.com>
2024-08-12 14:05:03 -0700

    Added subway markers

--- map.js
+++ map.js
@@ -13,3 +13,27 @@ fetch('data/Green Spaces.geojson')
         L.geoJSON(data).addTo(map);
     })
     .catch(error => console.log('Error loading GeoJSON:', error));
+
+    // Load and add the stations GeoJSON layer
+    fetch('data/stations.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, {
+                    icon: L.icon({
+                        iconUrl: 'icons\Subway-Icon.png', // Path to your custom icon
+                        iconSize: [32, 32], // Size of the icon, adjust as needed
+                        iconAnchor: [16, 32], // Anchor position of the icon, adjust as needed
+                        popupAnchor: [0, -32] // Position of the popup relative to the icon
+                    })
+                });
+            },
+            onEachFeature: function (feature, layer) {
+                if (feature.properties && feature.properties.name) {
+                    layer.bindPopup('<h3>' + feature.properties.name + '</h3>');
+                }
+            }
+        }).addTo(map);
+    })
+    .catch(error => console.log('Error loading stations GeoJSON:', error));
