817e7655296a1c129ae3dadba353503978120a5e
hoornaert-cj <97850176+hoornaert-cj@users.noreply.github.com>
2024-08-13 14:37:19 -0700

    Added dropdown menu with names of subway stations

--- map.js
+++ map.js
@@ -5,35 +5,44 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+let greenSpacesLayer, stationsLayer;
 
 // Load GeoJSON from an external file
 fetch('data/Green Spaces.geojson')
     .then(response => response.json())
     .then(data => {
-        L.geoJSON(data).addTo(map);
+        greenSpacesLayer = L.geoJSON(data).addTo(map);
     })
     .catch(error => console.log('Error loading GeoJSON:', error));
 
     // Load and add the stations GeoJSON layer
     fetch('data/stations.geojson')
     .then(response => response.json())
-    .then(data => {
-        L.geoJSON(data, {
+    .then(stationsData => {
+        stationsLayer = L.geoJSON(stationsData, {
             pointToLayer: function (feature, latlng) {
                 return L.marker(latlng, {
                     icon: L.icon({
-                        iconUrl: 'icons\Subway-Icon.png', // Path to your custom icon
-                        iconSize: [32, 32], // Size of the icon, adjust as needed
-                        iconAnchor: [16, 32], // Anchor position of the icon, adjust as needed
-                        popupAnchor: [0, -32] // Position of the popup relative to the icon
+                        iconUrl: 'icons/Subway-Icon.png',
+                        iconSize: [32, 32],
+                        iconAnchor: [16, 32],
+                        popupAnchor: [0, -32]
                     })
                 });
-            },
-            onEachFeature: function (feature, layer) {
-                if (feature.properties && feature.properties.name) {
-                    layer.bindPopup('<h3>' + feature.properties.name + '</h3>');
-                }
             }
         }).addTo(map);
+
+        // Populate the dropdown menu with station names
+        populateStationDropdown(stationsData);
     })
-    .catch(error => console.log('Error loading stations GeoJSON:', error));
+    .catch(error => console.log('Error loading Stations GeoJSON:', error));
+
+    function populateStationDropdown(stationsData) {
+        const dropdown = document.getElementById('station-dropdown');
+        stationsData.features.forEach((station, index) => {
+            const option = document.createElement('option');
+            option.value = index;
+            option.text = station.properties.stop_name
+            dropdown.add(option);
+        });
+    }
